7949a554143b62f9a95f7c080eb34b55fd58c3e0
jasminekbal <59400398+jasminekbal@users.noreply.github.com>
2020-08-08 02:51:05 -0400

    Update Player.h

--- Characters/Player.h
+++ Characters/Player.h
@@ -1,22 +1,24 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include <memory>
-#include "../Info.h"
+#include "../Ground.h"
 
 class Tile;
 class Enemy;
 class Gold;
 class Potion;
-class Ground;
 
+//Invariant: hp must be between 0 and maxHealth or if maxHealth is -1 then it has no upper bound
 class Player {
     private:
     char c = '@';
     std::shared_ptr<Ground> location;
     int baseAtk, baseDef;
+    int checkHp(int hp) const;
 
     protected:
     int hp, atk, def, score, maxHealth;
+    void changeHp(int newHp);
     
     public:
     Player(int hp, int atk, int def, int maxHealth); //inital constructor
@@ -30,11 +32,11 @@ class Player {
     int getBaseAtk();
     int getBaseDef();
     std::shared_ptr<Ground> getLocation(); //called by game so the game knows where the player is
-    void setLocation( std::shared_ptr<Ground> tile ); //called whenever the player moves to a new tile
+    void setLocation( std::shared_ptr<Ground> g ); //called whenever the player moves to a new tile
 
     virtual bool attack(Enemy &e) = 0;  //might return a string if we want to go that route
     virtual bool onAttacked(Enemy &e)= 0;
-    virtual void newPlayer(); //called when there is a new floor
+    void newPlayer(); //called when there is a new floor
     
     void collectGold(std::shared_ptr<Gold> g);
 
