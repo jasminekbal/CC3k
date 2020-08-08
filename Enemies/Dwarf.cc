b1bbc9032de3f1c96d1658a5f51e6497391fafb7
jasminekbal <59400398+jasminekbal@users.noreply.github.com>
2020-08-08 03:02:28 -0400

    Create Dwarf.cc

---
+++ Enemies/Dwarf.cc
@@ -0,0 +1,13 @@
+#include "Dwarf.h"
+
+
+Dwarf::Dwarf(): Enemy(100, 20, 30, 'W'){}
+
+Dwarf::~Dwarf(){}
+
+
+//will do later
+bool Dwarf::attack(Player &p){}
+
+bool Dwarf::onAttacked(Player &p){}
+
