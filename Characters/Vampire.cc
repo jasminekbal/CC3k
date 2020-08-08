d27c3738a0c432f53e564e24c3dcfd2995d77a8c
jasminekbal <59400398+jasminekbal@users.noreply.github.com>
2020-08-08 02:54:24 -0400

    Create Vampire.cc

---
+++ Characters/Vampire.cc
@@ -0,0 +1,16 @@
+#include "Vampire.h"
+
+Vampire::Vampire(): Player(50, 25, 25, -1){}
+
+Vampire::~Vampire(){
+
+}
+
+//will do later
+bool Vampire::attack(Enemy &e){
+
+}
+
+bool Vampire::attack(Dwarf &e);
+
+bool Vampire::onAttacked(Enemy &) ;
