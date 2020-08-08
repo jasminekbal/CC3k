2d0d7471aa4d68fe640c0e902db9bc3703fc7df3
jasminekbal <59400398+jasminekbal@users.noreply.github.com>
2020-08-08 02:57:49 -0400

    Update Enemy.cc

--- Enemies/Enemy.cc
+++ Enemies/Enemy.cc
@@ -1,12 +1,46 @@
 #include "Enemy.h"
-using namespace std;
+#include "../Items/Gold.h"
 
-Enemy::Enemy(int hp, int atk, int def, char c){}
-bool Enemy::attack(Player &p){ return 0; } 
-bool Enemy::onAttacked(Player &p){ return 0; }
-std::shared_ptr<Gold> Enemy::onDeath(){ return make_shared<Gold>(1, 1); }
+//will do later
+void Enemy::setGold( std::shared_ptr<Gold> gP ){
 
-int Enemy::getHp() const{ return hp; }
-int Enemy::getAtk() const{ return atk; }
-int Enemy::getDef() const{ return def; }
-char Enemy::getChar() const{ return c; }
+}
+
+void Enemy::setHP(int hp){
+    hp = hp;
+}
+void Enemy::setAtk(int atk){
+    atk = atk;
+}
+void Enemy::setDef(int def){
+    def = def;
+}
+
+Enemy::Enemy(int hp, int atk, int def, char c): hp(hp), atk(atk), def(def), c(c){}
+
+Enemy::~Enemy(){
+
+}
+       
+
+std::shared_ptr<Gold> Enemy::onDeath(){
+    return make_shared<Gold>(1, 1);
+}
+
+//will do later
+int Enemy::getHp() const{
+    return hp;
+}
+
+int Enemy::getAtk() const{
+    return atk;
+}
+
+int Enemy::getDef() const{
+    return def;
+}
+
+char Enemy::getChar() const{
+    return c;
+}
+
