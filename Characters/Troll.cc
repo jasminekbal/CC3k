6ec61b97247c2b2a1e3e9e74f20cc05f7c1bb6d0
jasminekbal <59400398+jasminekbal@users.noreply.github.com>
2020-08-08 02:53:58 -0400

    Create Troll.cc

---
+++ Characters/Troll.cc
@@ -0,0 +1,24 @@
+#include "Troll.h"
+
+Troll::Troll():Player(120, 25, 15, 120){
+
+}
+
+Troll::~Troll(){
+
+}
+
+
+//will do later
+bool Troll::attack(Enemy &e) {
+
+}
+
+bool Troll::onAttacked(Enemy &e){
+
+}
+
+bool Troll::onAttacked(Orc &o){
+
+}
+
