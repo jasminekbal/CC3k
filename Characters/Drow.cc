5f6bd26e15ef6e0e11d31bccb2ebf582e6e8bfaa
jasminekbal <59400398+jasminekbal@users.noreply.github.com>
2020-08-08 02:49:20 -0400

    Create Drow.cc

---
+++ Characters/Drow.cc
@@ -0,0 +1,20 @@
+#include "Drow.h"
+
+Drow::Drow():Player(150, 25, 15, 150){
+
+}
+
+Drow::~Drow(){
+
+}
+
+
+//will do later
+bool Drow::attack(Enemy &e){
+
+}
+
+bool Drow::onAttacked(Enemy &e){
+
+}
+
