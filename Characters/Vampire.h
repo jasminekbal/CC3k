1d24fed5d8a632a4f2291c94b26a6c50dbbf045a
Wenfei He <whe@Wenfeis-MacBook-Pro.local>
2020-08-12 14:47:02 -0700

    August 12 - abstract methods changed to override

--- Characters/Vampire.h
+++ Characters/Vampire.h
@@ -12,20 +12,20 @@ class Vampire: public Player {
     public:
     Vampire();
     virtual ~Vampire();
-    virtual bool attack(Dragon &e) = 0;  //might return a string if we want to go that route
-    virtual bool onAttacked(Dragon &e)= 0;
-    virtual bool attack(Dwarf &e) = 0;  //might return a string if we want to go that route
-    virtual bool onAttacked(Dwarf &e)= 0;
-    virtual bool attack(Elf &e) = 0;  //might return a string if we want to go that route
-    virtual bool onAttacked(Elf &e)= 0;
-    virtual bool attack(Halfling &e) = 0;  //might return a string if we want to go that route
-    virtual bool onAttacked(Halfling &e)= 0;
-    virtual bool attack(Human &e) = 0;  //might return a string if we want to go that route
-    virtual bool onAttacked(Human &e)= 0;
-    virtual bool attack(Merchant &e) = 0;  //might return a string if we want to go that route
-    virtual bool onAttacked(Merchant &e)= 0;
-    virtual bool attack(Orc &e) = 0;  //might return a string if we want to go that route
-    virtual bool onAttacked(Orc &e)= 0;
+    virtual bool attack(Dragon &e) override;  //might return a string if we want to go that route
+    virtual bool onAttacked(Dragon &e) override;
+    virtual bool attack(Dwarf &e) override;  //might return a string if we want to go that route
+    virtual bool onAttacked(Dwarf &e) override;
+    virtual bool attack(Elf &e) override;  //might return a string if we want to go that route
+    virtual bool onAttacked(Elf &e) override;
+    virtual bool attack(Halfling &e) override;  //might return a string if we want to go that route
+    virtual bool onAttacked(Halfling &e) override;
+    virtual bool attack(Human &e) override;  //might return a string if we want to go that route
+    virtual bool onAttacked(Human &e) override;
+    virtual bool attack(Merchant &e) override;  //might return a string if we want to go that route
+    virtual bool onAttacked(Merchant &e) override;
+    virtual bool attack(Orc &e) override;  //might return a string if we want to go that route
+    virtual bool onAttacked(Orc &e) override;
 
 };
 
